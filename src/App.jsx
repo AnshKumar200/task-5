e08e25af9f1000c69b35b8038fb9e0a902f2f545
Ansh Kumar <124369727+AnshKumar200@users.noreply.github.com>
2024-11-02 19:19:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
       <div className="main">
       <Card name="Himanshu" position="Technical Lead" profile="pf1"/>
       <Card name="Ikshu" position="Technical Co-Lead" profile="pf2"/>
-      <Card name="Ansh" position="Club Member" profile="pf3"/>
+      <Card name="Ansh" position="Club Member(Not Yet) :)" profile="pf3"/>
       </div>
   )
 }
