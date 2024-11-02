dca1d7c78ca46796f40a727085e6899975f0c413
Ansh Kumar <124369727+AnshKumar200@users.noreply.github.com>
2024-11-02 19:18:09 +0530

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const Card = ({name, position, profile, inst="#", link="#", git="#"}) => { 
     return (
         <div className="cont">
-            <img src={`./../public/${profile}.jpg`} className="image"/>
+            <img src={`/${profile}.jpg`} className="image"/>
             <div className="name">{name}</div> 
             <div className="position">{position}</div>
             <div className="logo">
                 <a href={`https://www.instagram.com/${inst}`}>
-                    <img src="./../public/inst.png"/>    
+                    <img src="/inst.png"/>    
                 </a>
                 <a href={`https://www.lnikedin.com/in/${link}`}>
-                    <img src="./../public/link.png"/>    
+                    <img src="/link.png"/>    
                 </a>
                 <a href={`https://www.github.com/${git}`}>
-                    <img src="./../public/git.png"/>
+                    <img src="/git.png"/>
                 </a>
             </div>
         </div>  
